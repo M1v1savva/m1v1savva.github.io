ee713e143613623787dc0cb2e1cbe85838754722
Ivan Poliakov <ivan.polyakov.01@gmail.com>
2023-11-23 20:47:20 +0100

    Update Cache.jsx

--- src/components/Cache.jsx
+++ src/components/Cache.jsx
@@ -10,13 +10,6 @@ function Cache() {
 				</li>
 				<li>When I'm working on a project or solving a problem sometimes it can be hard to stop. 
 					<br/>But I already told you that I have a thing for programming.</li>
-				<li>My grandfather was an artist. I draw and <s>almost forgot how to</s> play piano. 
-				<br/>As cliche as it sounds, programming is like art to me: 
-				be it development, or algorithmic problem, or building a DL model.</li>
-				<li>In 2014 I saw Leo Messi play on TV and have been a massive football fan since. 
-					Playing and watching.</li>
-				<li>My views on consciousness and free will are largely inspired by Joscha Bach.</li>
-				<li>I agree with the perspective that many economic and social issues are structural in nature.</li>
 			</ul>
 		</div>
 	)
